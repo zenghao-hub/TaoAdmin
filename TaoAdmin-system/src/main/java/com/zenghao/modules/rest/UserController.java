c0f6edf03094d44bfdd10a4c058fbda2545ad4e5
“zenghao-hub” <15527597628@163.com>
2021-08-12 22:57:09 +0800

    初始化

--- TaoAdmin-system/src/main/java/com/zenghao/modules/rest/UserController.java
+++ TaoAdmin-system/src/main/java/com/zenghao/modules/rest/UserController.java
@@ -3,6 +3,8 @@
  */
 package com.zenghao.modules.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 	@GetMapping(value = "/test")
-	public String test() {
-		return "success";
+	public ResponseEntity<Object> test() {
+		return new ResponseEntity<>("success", HttpStatus.OK);
 	}
 }
